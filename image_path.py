0ff2d21bd4cff6a0f6e7fa16274b5ad9084ab15f
Dominik Roczan <70064951+DominikRoczan@users.noreply.github.com>
2024-04-02 22:05:56 +0200

    update

--- image_path.py
+++ image_path.py
@@ -1,14 +1,17 @@
-# image_path = 'new_Image/107.jpg'
-image_path = 'circles_test/CIRCLES_115.jpg'
+image_path = 'new_Image/107.jpg'
 
-# # Wydruk wszystkich pikseli
-# import cv2
-# import numpy as np
-#
-# image = cv2.imread(image_path)
-# print(image)
-# print(image.shape)
-#
-# with open('piksele_obrazu.txt', 'w') as file:
-#     for row in image:
-#         np.savetxt(file, row)
+
+def print_allpixels():
+    import cv2
+    import numpy as np
+
+    image = cv2.imread(image_path)
+    print(image)
+    print(image.shape)
+
+    with open('all_pxels.txt', 'w') as file:
+        for row in image:
+            np.savetxt(file, row)
+
+if __name__ == '__main__':
+    print_allpixels()
