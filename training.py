824451f7f30da1f562e7afeb1521e7dd8fed62db
domin <dominik.roczan@gmail.com>
2024-04-02 21:53:05 +0200

    update

---
+++ training.py
@@ -0,0 +1,299 @@
+# tensorflow
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+
+# Callbacks
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
+
+# metrics
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+
+import numpy as np
+import os
+from datetime import datetime
+
+
+def path_datasets():
+    """"Path for data"""
+    base_dir = f'G:/Mój dysk/10_Machine Learning/00_Projekty/Data_Sets/TRvsCI'
+    # base_dir = f'G:/Mój dysk/10_Machine Learning/00_Projekty/Data_Sets/images'
+
+    train_dir = os.path.join(base_dir, 'train')
+    val_dir = os.path.join(base_dir, 'val')
+    test_dir = os.path.join(base_dir, 'test')
+
+    return train_dir, val_dir, test_dir
+
+
+def data_generator():
+    """Data generators configuration"""
+    train_datagen = (ImageDataGenerator
+                     (rescale=1. / 255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
+                      shear_range=0.2, zoom_range=0.2, horizontal_flip=True, fill_mode='nearest'
+                      ))
+    val_datagen = (ImageDataGenerator(rescale=1. / 255))
+    test_datagen = (ImageDataGenerator(rescale=1. / 255))
+    return train_datagen, val_datagen, test_datagen
+
+
+def data_processing(train_dir, val_dir, test_dir, class_mode='binary', batch_size=16):
+    """Data collection and processing"""
+
+    train_generator = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(
+        train_dir,
+        target_size=(
+            224, 224),
+        classes=[
+            'circles',
+            'triangles'],
+        batch_size=batch_size,
+        shuffle=True,
+        class_mode=class_mode)
+    val_generator = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(val_dir,
+                                                                                                    target_size=(
+                                                                                                        224, 224),
+                                                                                                    classes=['circles',
+                                                                                                             'triangles'],
+                                                                                                    batch_size=batch_size,
+                                                                                                    shuffle=False,
+                                                                                                    class_mode=class_mode)
+    test_generator = ImageDataGenerator(preprocessing_function=preprocess_input).flow_from_directory(test_dir,
+                                                                                                     target_size=(
+                                                                                                         224, 224),
+                                                                                                     classes=['circles',
+                                                                                                              'triangles'],
+                                                                                                     batch_size=batch_size,
+                                                                                                     shuffle=False,
+                                                                                                     class_mode=class_mode)
+
+    return train_generator, val_generator, test_generator, class_mode, batch_size
+
+
+base_model = MobileNetV2(input_shape=(224, 224, 3), include_top=False, weights='imagenet')
+
+# Nazwa modelu
+model_name = base_model.name
+model_name = model_name[:-11]
+
+# Zamrożenie wag modelu bazowego
+base_model.trainable = False
+
+# Budowa model
+model = Sequential([
+    base_model,
+    layers.Conv2D(8, (2, 2), activation='sigmoid', padding='same'),
+    layers.MaxPooling2D((1, 1)),
+    layers.BatchNormalization(),
+
+    layers.Conv2D(16, (2, 2), activation='sigmoid', padding='same'),
+    layers.MaxPooling2D((2, 2)),
+    layers.BatchNormalization(),
+
+    layers.Conv2D(32, (16, 16), activation='sigmoid', padding='same'),
+    layers.MaxPooling2D((2, 2)),
+    layers.BatchNormalization(),
+
+    layers.Conv2D(64, (16, 16), activation='relu', padding='same'),
+    layers.MaxPooling2D((1, 1)),
+    layers.BatchNormalization(),
+
+    layers.Flatten(),
+    layers.Dense(16, activation='relu'),
+
+    # layers.Dropout(0.25),
+    layers.Dense(1, activation='sigmoid')
+])
+# model.summary()
+
+# Kompilacja modelu
+model.compile(optimizer=Adam(learning_rate=0.0001),
+              loss=BinaryCrossentropy(),
+              metrics=['accuracy']
+              )
+
+# Folder na wyniki
+result_folder = 'results'
+os.makedirs(result_folder, exist_ok=True)
+
+# Nazwa pliku tekstowego
+result_file_name = f"{model_name}+2Classes+++working.txt"
+result_file_path = os.path.join(result_folder, result_file_name)
+
+# Directory TensorBoard
+log_dir = (f'C:/USERS/domin/OneDrive/Pulpit/Python/logs/'
+           f'{model_name}....{datetime.now().strftime("%Y.%m.%d....%H.%M")}')
+# log_dir = (f'E:/USERS/dominik.roczan/PycharmProjects/logs/'
+#          f'{model_name}....{datetime.now().strftime("%Y.%m.%d....%H.%M")}')
+
+os.makedirs(log_dir, exist_ok=True)
+
+# TensorBoard Callback
+tensorboard_train = TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=False, write_images=False)
+
+# Wywołanie tensorboard w konsoli: tensorboard --logdir=C:/USERS/domin/OneDrive/Pulpit/Python/logs
+# Wywołanie tensorboard w konsoli: tensorboard --logdir=E:/USERS/dominik.roczan/PycharmProjects/logs
+
+
+# Directory Checkpoint
+checkpoint_filepath = 'results/checkpoint.model.keras'
+
+
+def model_checkpoint_callback():
+    return ModelCheckpoint(
+        filepath=checkpoint_filepath,
+        monitor='val_accuracy',
+        mode='max',
+        save_best_only=True,
+        verbose=1
+    )
+
+
+def early_stopping():
+    return EarlyStopping(
+        monitor="val_loss",
+        min_delta=0,
+        patience=3,
+        mode="min",
+        baseline=None,
+        restore_best_weights=False,
+        start_from_epoch=0,
+        verbose=1
+    )
+
+
+def train_model(train_generator, val_generator, batch_size=16):
+    # Początek czasu treningu
+    start_time = datetime.now()
+
+    # Trening modelu
+    model.fit_generator(generator=train_generator,
+                        steps_per_epoch=train_generator.n // batch_size,
+                        validation_data=val_generator,
+                        validation_steps=val_generator.n // batch_size,
+                        epochs=1,
+                        callbacks=[tensorboard_train, model_checkpoint_callback(), early_stopping()]
+                        )
+
+    # The model (that are considered the best) can be loaded as -
+    best_model = load_model('results/checkpoint.model.keras')
+
+    # Zapis modelu do pliku .h5
+    model.save(f'{model_name}+2_Classes++working.h5')
+    best_model.save(f'best.{model_name}+2_Classes++working5.h5')
+
+    # Podsumowanie modelu
+    best_model.summary()
+
+    # Koniec czasu treningu
+    end_time = datetime.now()
+    training_duration = end_time - start_time
+    save_model = datetime.now()
+    print(best_model)
+    return training_duration, save_model, best_model
+
+
+def ocena(test_generator, best_model):
+    # Pobierz dane do oceny modelu
+    y_true = test_generator.labels  # prawdziwe etykiety z generatora danych
+    y_pred = np.argmax(best_model.predict(test_generator), axis=1)  # przewidziane etykiety modelu
+    y_pred_binary = (best_model.predict(test_generator) > 0.5).astype(int)
+
+    return y_true, y_pred, y_pred_binary
+
+
+def ocena_oceny(y_true, y_pred_binary):
+    # Ocena modelu
+    classification_rep = classification_report(y_true, y_pred_binary)
+    conf_matrix = confusion_matrix(y_true, y_pred_binary)
+    accuracy = accuracy_score(y_true, y_pred_binary)
+
+    return classification_rep, conf_matrix, accuracy
+
+
+def prints(best_model, train_generator, val_generator, test_generator, training_duration, save_model,
+           classification_rep, conf_matrix, accuracy):
+    train_results = best_model.evaluate(train_generator)
+    loss_train_results = round(train_results[0], 4)
+    acc_train_results = round(train_results[1], 4)
+    print("Train Loss:", loss_train_results)
+    print("Train Accuracy:", acc_train_results)
+
+    # Wyniki na zbiorze walidacyjnym
+    val_results = best_model.evaluate(val_generator)
+    loss_val_results = round(val_results[0], 4)
+    acc_val_results = round(val_results[1], 4)
+    print("Validation Loss:", loss_val_results)
+    print("Validation Accuracy:", acc_val_results)
+
+    # Wyniki na zbiorze testowym
+    test_results = best_model.evaluate(test_generator)
+    loss_test_results = round(test_results[0], 4)
+    acc_test_results = round(test_results[1], 4)
+    print("Test Loss:", loss_test_results)
+    print("Test Accuracy:", acc_test_results)
+
+    # Zapis wyników do pliku .txt
+    with open(result_file_path, 'w') as result_file:
+        result_file.write("Model Name: {}\n".format(model_name))
+        result_file.write("\n")
+        result_file.write('Data: {}\n'.format(save_model))
+        result_file.write("Training Duration: {}\n".format(training_duration))
+        result_file.write("\n")
+        result_file.write("Train Accuracy: {}\n".format(acc_train_results))
+        result_file.write("Validation Accuracy: {}\n".format(acc_val_results))
+        result_file.write("Test Accuracy: {}\n".format(acc_test_results))
+        result_file.write("\n")
+        result_file.write("Train loss: {}\n".format(loss_train_results))
+        result_file.write("Validation loss: {}\n".format(loss_val_results))
+        result_file.write("Test loss: {}\n".format(loss_test_results))
+        result_file.write("\n\n")
+        result_file.write("Classification Report:\n")
+        result_file.write(classification_rep)
+        result_file.write("\n\n")
+        result_file.write("Confusion Matrix:\n")
+        result_file.write(np.array2string(conf_matrix, separator=', '))
+
+    # Otrzymaj słownik przypisujący etykiety klas do indeksów
+    class_indices = train_generator.class_indices
+
+    # Wydrukuj słownik
+    print("Class Indices:", class_indices)
+
+    # Otrzymaj odwrotny słownik, przypisujący indeksy do etykiet
+    indices_to_classes = {v: k for k, v in class_indices.items()}
+
+    # Wydrukuj odwrotny słownik
+    print("Indices to Classes:", indices_to_classes)
+
+    # Otrzymaj listę klas
+    class_names = list(class_indices.keys())
+
+    # Wydrukuj listę klas
+    print("Class Names:", class_names)
+
+
+if __name__ == "__main__":
+    train_dir, val_dir, test_dir = path_datasets()
+
+    train_datagen, val_datagen, test_datagen = data_generator()
+    data_processing(train_dir, val_dir, test_dir, class_mode='binary', batch_size=16)
+    train_generator, val_generator, test_generator, class_mode, batch_size = data_processing(train_dir, val_dir,
+                                                                                             test_dir,
+                                                                                             class_mode='binary',
+                                                                                             batch_size=16)
+    early_stop = early_stopping()
+    checkpoint = model_checkpoint_callback()
+
+    training_duration, save_model, best_model = train_model(train_generator, val_generator, batch_size)
+
+    y_true, y_pred, y_pred_binary = ocena(test_generator, best_model)
+    
+    classification_rep, conf_matrix, accuracy = ocena_oceny(y_true, y_pred_binary)
+
+    prints(best_model, train_generator, val_generator, test_generator, training_duration, save_model,
+           classification_rep, conf_matrix, accuracy)
